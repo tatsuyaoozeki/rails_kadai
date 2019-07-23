655ec5430551b7dea211aba0ac42674d94ec8501
tatsuya oozeki <tasuya6281@icloud.com>
2019-07-23 10:50:44 +0900

    createアクションにparamsメソッドとredirect作成

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -5,4 +5,9 @@ class MainsController < ApplicationController
   def new
     @main = Main.new
   end
+
+  def create
+    Main.create(content: params[:main][:content])
+    redirect_to "new_main_path"
+  end
 end
